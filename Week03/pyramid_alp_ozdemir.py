5d81ebab41727809793aa0da8a19cde0f66a8e6e
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2026-03-24 12:11:49 +0300

    Update pyramid_alp_ozdemir.py

--- Week03/pyramid_alp_ozdemir.py
+++ Week03/pyramid_alp_ozdemir.py
@@ -1,6 +1,8 @@
 def calculate_pyramid_height(blocks):
     layer = 0
+    height = 0
     while blocks >= layer + 1 :
         blocks -= layer
         layer += 1
-    return layer
+        height += 1
+    return height
